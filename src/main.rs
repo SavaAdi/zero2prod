b7efc5ee22121c9a053c02296cda6b53b94fef0b
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2021-11-30 21:20:36 +0200

    Add subscription skeleton endpoint (+disabled random port binding)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use zero2prod::run;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
-    println!("TcpListener {}", listener.local_addr().unwrap());
 
     run(listener)?.await
 }
