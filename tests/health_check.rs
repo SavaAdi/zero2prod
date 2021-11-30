2e49f10d7e11691850a79be2ba9f79d157657fde
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2021-11-30 19:21:24 +0200

    Fix linting

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,7 +2,6 @@ use std::net::TcpListener;
 
 #[actix_rt::test]
 async fn health_check_works() {
-
     let address = spawn_app();
     let client = reqwest::Client::new();
 
@@ -18,13 +17,8 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-// No .await call, therefore no need for `spawn_app` to be async now.
-// We are also running tests, so it is not worth it to propagate errors:
-// if we fail to perform the required setup we can just panic and crash
-// all the things.
-fn spawn_app() -> String{
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
 
     let server = zero2prod::run(listener).expect("Failed to bind address");
