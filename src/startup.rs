f5e1985d21d7cb986a2406d29e4f695c1a074bad
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2021-12-02 20:09:41 +0200

    Set up proper integration testing

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,21 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(_listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // Wrap the pool using web::Data, which boils down to an Arc smart pointer
+    let db_pool = web::Data::new(db_pool);
+
+    let server = HttpServer::new(move || {
         App::new()
             .route("/subscriptions", web::post().to(subscribe))
             .route("/health_check", web::get().to(health_check))
+            // Register the connection as part of the application state
+            .app_data(db_pool.clone())
     })
-    .bind("localhost:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
