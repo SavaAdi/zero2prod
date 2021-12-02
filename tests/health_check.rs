c84a03115d3ee0ea259f64080b47593f01d36d1a
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2021-12-02 17:22:04 +0200

    Update project and CI pipeline

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,7 @@
 use std::net::TcpListener;
+use zero2prod::startup;
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
 
 #[actix_rt::test]
 async fn health_check_works() {
@@ -21,8 +24,18 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
 // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+
+// The `Connection` trait MUST be in scope for us to invoke
+// `PgConnection::connect` - it is not an inherent method of the struct!
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
 // Act
     let response = client
         .post(&format!("{}/subscriptions", &app_address))
@@ -31,8 +44,17 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .send()
         .await
         .expect("Failed to execute request.");
+
 // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[actix_rt::test]
@@ -54,6 +76,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             .send()
             .await
             .expect("Failed to execute request.");
+
 // Assert
         assert_eq!(
             400,
@@ -69,7 +92,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // let port = listener.local_addr().unwrap().port();
 
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     // format!("http://127.0.0.1:{}", port)
