763521c2988c5bc55b765ac8909db70c970a8697
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2021-12-06 19:16:14 +0200

    Add log info for integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+use sqlx::postgres::PgPool;
+use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use sqlx::postgres::PgPool;
-use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     // Panic if we can't read configuration
