30ea8061d8481fd53e2ccc9aba2ab0968f54d227
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2021-12-26 21:38:04 +0200

    Add migration for new table

--- migrations/20211226191501_make_status_not_null_in_subscriptions.sql
+++ migrations/20211226191501_make_status_not_null_in_subscriptions.sql
@@ -1,8 +1,8 @@
 BEGIN;
 -- Backfill `status` for historical entries
 UPDATE subscriptions
-SET status = 'confirmed'
-WHERE status IS NULL;
--- Make `status` mandatory
-ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
+    SET status = 'confirmed'
+    WHERE status IS NULL;
+    -- Make `status` mandatory
+    ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
 COMMIT;
