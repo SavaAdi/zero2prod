4583326198b7c8b67be2f82b83fccce868d89e65
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2022-03-13 21:01:23 +0200

    Refactor logic for fetching confirmed subscribers

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -74,16 +74,7 @@ struct ConfirmedSubscriber {
 async fn get_confirmed_subscribers(
     pool: &PgPool,
 ) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
-    // We only need `Row` to map the data coming out of this query.
-    // Nesting its definition inside the function itself is a simple way
-    // to clearly communicate this coupling (and to ensure it doesn't
-    // get used elsewhere by mistake).
-    struct Row {
-        email: String,
-    }
-
-    let rows = sqlx::query_as!(
-        Row,
+    let confirmed_subscribers = sqlx::query!(
         r#"
         SELECT email
         FROM subscriptions
@@ -91,17 +82,14 @@ async fn get_confirmed_subscribers(
         "#,
     )
     .fetch_all(pool)
-    .await?;
-
-    // Map into the domain type
-    let confirmed_subscribers = rows
-        .into_iter()
-        // No longer using `filter_map`!
-        .map(|r| match SubscriberEmail::parse(r.email) {
-            Ok(email) => Ok(ConfirmedSubscriber { email }),
-            Err(error) => Err(anyhow::anyhow!(error)),
-        })
-        .collect();
+    .await?
+    .into_iter()
+    // No longer using `filter_map`!
+    .map(|r| match SubscriberEmail::parse(r.email) {
+        Ok(email) => Ok(ConfirmedSubscriber { email }),
+        Err(error) => Err(anyhow::anyhow!(error)),
+    })
+    .collect();
 
     Ok(confirmed_subscribers)
 }
